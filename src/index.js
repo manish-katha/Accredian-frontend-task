4b2d8715e54da5a29f462267229e4e6ff16ce43b
manish-katha <manish.sh.katha@gmail.com>
2024-07-21 23:55:27 +0530

    server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-export const server = "http://localhost:4000/api/v1";
+export const server = "https://accredian-backend-task-opgm.onrender.com/api/v1";
 root.render(
   <React.StrictMode>
     <App />
